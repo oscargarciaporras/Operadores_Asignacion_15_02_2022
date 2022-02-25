cad72ae76268b63f4a822a7686a82535d5a1f57d
oscargarciaporras <osligarpo92@gmail.com>
2022-02-24 19:48:24 -0500

    Operador de comparación Igualdad

--- main.js
+++ main.js
@@ -1,90 +1,13 @@
-//piramide de prioridades
+// Operador de comparación Igualdad
+let x = 5+(2*2)-(6/2);
+//      5+ (4) - (6/2)
+//      5+ (4) -(3)
+//      9     -  3
+//           6
+let y = 5;
 
-//let reslutado2  = 5+1*8+(9 * (5+8) / 2);
-//let reslutado2  = 5+1*8+(9 * 13 / 2);
-// let reslutado2  = 5+1*8+(117/2);
-//let reslutado2  = 5+1*8+(58.5);
-//let reslutado2  = 5+8+58.5;
-// let reslutado2  = 71.5;
-
-
-// console.log(reslutado2);
-
-// let reslutado  = 5*((4/4)*(9-7)*2)+6;
-// let reslutado  = 5*(1*2*2)+6;
-// let reslutado  = 5*(4)+6;
-// let reslutado  = 20+6;
-// let reslutado  = 20+6;
-// let reslutado  = 26
-
-// console.log(reslutado);
-
-
-
-
-
-
-
-
-// // Operadores Arigmetico asignación de resto
-// let X = parseInt(prompt(" ingrese el numero ", 0));
-// let Y = new Number(prompt(" ingrese el numero ", 0));
-// let myStyle = `
-//     color: white;
-//     background: #801515;
-//     padding: 5px;
-//     border-radius: 5px;
-// `;
-
-// console.group(`%cOperadores Arigmeticos asignación de resto`, myStyle);
-
-//     console.log(`Variable X: ${X} Y: ${Y}`);
-//     X %= Y;
-//     console.log(`La asignación de resto es: ${X}`);
-
-// console.groupEnd();
-
-
-
-
-
-
-
-
-// let nombre = prompt(`Ingrese el nombre del usuario`, `Usuario ingrese su nombre porfavor`);
-// console.log(nombre);
-
-
-
-
-// // Operadores Adición
-// let x = null;
-// let y = 50;
-// let myStyle = `
-//     color: #fff;
-//     background: #226666;
-//     padding: 5px;
-//     border-radius: 5px;
-// `;
-// // Adición
-// console.group(`%cOperadores Adición`, myStyle);
-
-
-//     console.log(`Variable x: ${x} Variable y: ${y}`);
-
-//     // Abreviacion
-//     console.log(`Abreviacion: x += y;`);
-//     x += y;
-
-//     // // Significado
-//     // console.log(`Significado: x = x + y;`);
-//     // x = x + y;
-
-//     console.log(`x: ${x} y: ${y}`);
-
-
-
-// console.groupEnd();
+let resultado = x == y;
+console.log(`Variable x: ${x} y: ${y} Resultado '==' ${resultado}`);
 
 
 
@@ -94,27 +17,18 @@
 
 
 
-// // Operadores Asignacion
-// let x;
-// let y = 50;
-// let myStyle = `
-//     color: #fff;
-//     background: #226666;
-//     padding: 5px;
-//     border-radius: 5px;
-// `;
-
-// // Asignacion
-// console.group(`%cOperadores Asignacion`, myStyle);
-
-
-//     console.log(`Variable x: ${x} Variable y: ${y}`);
-
-//     // Abreviacion
-//     console.log(`Abreviacion: x = y;`);
-//     x = y;
-//     console.log(`x: ${x} y: ${y}`);
 
 
 
-// console.groupEnd();
+
+
+
+
+
+
+
+
+
+
+
+
