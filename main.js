8eaf88e5af92060754ec331db5156b80b10abb79
oscargarciaporras <osligarpo92@gmail.com>
2022-02-24 20:12:04 -0500

    Operador de comparacion Diferente

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-// Operador de comparación Igualdad
-let x = 5+(2*2)-(6/2);
-//      5+ (4) - (6/2)
-//      5+ (4) -(3)
-//      9     -  3
-//           6
-let y = 5;
+// Operador de comparacion Diferente
+let x = 5*2;
+//       10
+//
+//
+//
+let y = (5/5-(3+9) - (5%3)*2);
+//       (5/5-(12) - (2)*2);
+//       (5/5-(12) - (2)*2);
+//       (5/5-(12) - 4);
+//         (1-12 - 4)
+//        (-11 - 4);
+//         -15
+let resultado = x != y;
 
-let resultado = x == y;
-console.log(`Variable x: ${x} y: ${y} Resultado '==' ${resultado}`);
-
-
-
-
-
+console.log(`Variable x: ${x} y: ${y} Diferente '!=' ${resultado}`);
 
 
 
