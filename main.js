0b8b6c01a829a64a45573c7bf205097fb89b17dd
oscargarciaporras <osligarpo92@gmail.com>
2022-03-03 19:16:31 -0500

    Diseño y Operador logico NOT

--- main.js
+++ main.js
@@ -1,60 +1,73 @@
-let a = true;
+let a = false;
 let b = false;
 let validar = function(id, ope){
     document.querySelector(id).insertAdjacentHTML("beforeend",
     `
         <tr>
             <td>${a}</td>
-            <td>${b}</td>
+            ${(ope != "!") ? `<td>${b}</td>` : ""}
             <td>${
                 (ope == "&") ? a && b 
-                : (ope == "|") ? a || b 
+                : (ope == "|") ? a || b                 
+                : (ope == "!") ? !a
                 : ""
             }</td>
+        </tr>
+        <tr>
+            ${(ope != "!") ? `<td>${a}</td><td>${!b}</td>` : `<td>${!a}</td>`}
+            <td>${
+                (ope == "&") ? a && !b 
+                : (ope == "|") ? a || !b                 
+                : (ope == "!") ? !!a
+                : ""
+            }</td>
+        </tr>
+        <tr>
+            ${(ope != "!") ? `<td>${!a}</td><td>${b}</td>` : ""}
+            ${
+                (ope == "&") ? `<td>${!a && b}</td>` 
+                : (ope == "|") ? `<td>${!a || b}</td>`                 
+                : ""
+            }
+        </tr>
+        <tr>
+            ${(ope != "!") ? `<td>${!a}</td><td>${!b}</td>` : ""}
+            ${
+                (ope == "&") ? `<td>${!a && !b}</td>` 
+                : (ope == "|") ? `<td>${!a || !b}</td>`                 
+                : ""
+            }
         </tr>
     `
     );
 };
 validar("#OperadorAND", "&");
 validar("#OperadorOR", "|");
-
-console.log(!false);
-
+validar("#OperadorNOT", "!");
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Ejemplo
+// (ope == "&") ? a && b 
+// : (ope == "|") ? a || b                 
+// : (ope == "!") ? !a
+// : ""
+// /////
+// function resultado(){
+//     if(ope == "&"){
+//         return a && b;
+//     }else{
+//         if(ope == "|"){
+//             return  a || b;
+//         }else{
+//             if(ope == "!"){
+//                 return !a;
+//             }else{
+//                 return "";
+//             }
+//         }
+//     }
+// }
 
 
 
